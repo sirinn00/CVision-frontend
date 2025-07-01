2de8874679c7b50eb61dbc93c33da3f670242a9d
Ayşenur Şirin <aysenursirin0@gmail.com>
2025-07-01 19:27:25 +0300

    deleted unnecessary children prop

--- src/components/atoms/DescriptionText.jsx
+++ src/components/atoms/DescriptionText.jsx
@@ -3,10 +3,11 @@ import { Typography } from 'antd';
 
 const { Paragraph } = Typography;
 
-const DescriptionText = ({ children }) => (
+const DescriptionText = () => (
   <Paragraph style={{ fontSize: '1.2rem', textAlign: 'center', maxWidth: 800 }}>
     Bu platformda,
     yüklediğiniz CV üzerinden akademik başarılarınızı, iş geçmişinizi ve teknik becerilerinizi değerlendirebilirsiniz.
   </Paragraph>
 );
+
 export default DescriptionText;
