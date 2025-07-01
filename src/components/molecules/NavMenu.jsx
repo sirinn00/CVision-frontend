9f3c3709b668fd76f7831aa3bd69ec45eb5d3741
Ayşenur Şirin <aysenursirin0@gmail.com>
2025-07-01 15:59:47 +0300

    implemented primary button as an atomic component

--- src/components/molecules/NavMenu.jsx
+++ src/components/molecules/NavMenu.jsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import { Menu, Button } from 'antd';
+import NavigationMenu from '../atoms/NavigationMenu';
+import PrimaryButton from '../atoms/PrimaryButton';
 
-const NavMenu = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        gap: 16,
-        alignItems: 'center',
-      }}
-    >
-      <Menu
-        mode="horizontal"
-        style={{
-          background: 'transparent',
-          flex: '1 1 auto',
-          minWidth: 200,
-        }}
-      >
-        <Menu.Item key="1">Ana Sayfa</Menu.Item>
-        <Menu.Item key="2">Nasıl Çalışır?</Menu.Item>
-      </Menu>
-
-      <Button type="primary">CV Tara</Button>
-    </div>
-  );
-};
+const NavMenu = () => (
+  <div
+    style={{
+      display: 'flex',
+      gap: 16,
+      alignItems: 'center',
+    }}
+  >
+    <NavigationMenu />
+    <PrimaryButton>CV Tara</PrimaryButton>
+  </div>
+);
 
 export default NavMenu;
