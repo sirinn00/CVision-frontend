1d36e07ad47b65bde0828e09e354424706b1165a
Ayşenur Şirin <aysenursirin0@gmail.com>
2025-07-01 19:34:28 +0300

    implemented "daha fazla bilgi" button

--- src/components/molecules/HeroButtons.jsx
+++ src/components/molecules/HeroButtons.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Button} from 'antd';
+import { Button } from 'antd';
 
 const HeroButtons = () => (
-    <Button type="primary" size="large">CV ŞABLONU İNDİR</Button>
+  <div style={{ display: 'flex', gap: '1rem', justifyContent: 'center' }}>
+    <Button type="primary" size="large">
+      CV ŞABLONU İNDİR
+    </Button>
+    <Button size="large">
+      Daha Fazla Bilgi
+    </Button>
+  </div>
 );
 
 export default HeroButtons;
