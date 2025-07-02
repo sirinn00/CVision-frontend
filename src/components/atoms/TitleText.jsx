583abe0bd0402af7f66e2c42f6ec54cfe6b6cd2e
Ayşenur Şirin <aysenursirin0@gmail.com>
2025-07-02 11:36:47 +0300

    refactor TitleText component to accept children as props

--- src/components/atoms/TitleText.jsx
+++ src/components/atoms/TitleText.jsx
@@ -3,9 +3,9 @@ import { Typography } from 'antd';
 
 const { Title } = Typography;
 
-const TitleText = () => (
+const TitleText = ({ children }) => (
   <Title level={1} style={{ fontSize: '3.2rem', textAlign: 'center', fontWeight: 800 }}>
-    CVision’a <span style={{ color: '#1677ff' }}>Hoş Geldiniz</span>
+    {children}
   </Title>
 );
 
