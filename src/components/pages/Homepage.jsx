7f794a31013d6a510204136c079468ad9a757b0d
Ayşenur Şirin <aysenursirin0@gmail.com>
2025-07-02 11:43:02 +0300

    update homepage layout style - global background color

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { Layout } from 'antd';
 import HeaderSection from '../organisms/HeaderSection';
 import HeroSection from '../organisms/HeroSection';
+import HowItWorksSection from '../organisms/HowItWorksSection';
 
 const HomePage = () => (
-  <Layout style={{ background: '#f0f2f5' }}>
+  <Layout
+    style={{
+      background: 'linear-gradient(to bottom right, #e6f0ff, #ffffff)',
+      minHeight: '100vh',
+    }}
+  >
     <HeaderSection />
     <HeroSection />
+    <HowItWorksSection />
   </Layout>
 );
 
